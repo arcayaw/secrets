6681c2ae143ad69e1f77f82343b1438c7665dc15
arcayaw <arcayaw@gmail.com>
2020-11-18 11:03:29 -0300

    adding passport authentication

--- app.js
+++ app.js
@@ -27,10 +27,18 @@ https://www.npmjs.com/package/mongoose-encryption
 //lo comento porque ahora vamos a usar bcrypt para robustecer el storage de contrasenias
 // const md5 = require("md5"); //https://www.npmjs.com/package/md5
 
-const bcrypt = require("bcrypt");
-const saltRounds = 10; // la cantidad de veces que se va a aplicar el proceso al hash para modificarlo
+/* comentamos la parte bcrypt porque ahora vamos a usar passport */
+// const bcrypt = require("bcrypt");
+// const saltRounds = 10; // la cantidad de veces que se va a aplicar el proceso al hash para modificarlo
 /* es un paquete que nos ayuda a fortalecer las passwords con rondas de salt  
 https://www.npmjs.com/package/bcrypt*/
+
+/* instalmos los paquetes necesario para passport y session */
+// npm i passport passport-local passport-local-mongoose express-session
+const session = require("express-session");
+//https://www.npmjs.com/package/express-session
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
@@ -42,11 +50,25 @@ app.use(
   })
 );
 
+//este app.use del session siempre debe ir aca, entre los otros app.use y la conexion a la bd
+app.use(
+  session({
+    secret: "Una Frase Larga Cualquiera.",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+//inicializacioms passport, esto es un metodo propio de passport
+app.use(passport.initialize());
+app.use(passport.session());
+
 /* dataBase creation and connection */
 mongoose.connect("mongodb://localhost:27017/userDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+mongoose.set("useCreateIndex", true); //para resolver el deprecationWarning de mongo-passport
 
 /*---------------------------------------------------------*/
 
@@ -74,9 +96,19 @@ const userSchema = new mongoose.Schema({
 // }); /* le podemos pasar como parametro solo el campo que queremos encriptar en este caso el password */
 //comentamos esto de nuevo porque ahora no vamos a usar el encryptation sino el hasd de md5
 
+//volvemos nuevamente a usar un plugin pero esta vez para passport
+userSchema.plugin(passportLocalMongoose);
+
 /*-------- creamos el model  --------*/
 
 const User = new mongoose.model("User", userSchema);
+
+//agregamos la estrategia de passport
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 /*---------------------------------------------------------*/
 
@@ -94,36 +126,48 @@ app.get("/register", function (req, res) {
   res.render("register");
 });
 /* targueteamos a la register page(/registter) para que renderee la pagina de registro */
+
+app.get("/secrets", function (req, res) {
+  if (req.isAuthenticated()) {
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  }
+});
+// si es autenticado correctamente con passport redirige a secrets, ifnot redirige a login de nuevo
+
+app.get("/logout", function (req, res) {
+  req.logout(); //desauntetico al usuario
+  res.redirect("/");
+});
 /*---------------------------------------------------------*/
 
 /* guardamos el nuevo usuario en el register*/
 
 /* Del form dentro de /register vamos a guardar el mail y el password*/
 app.post("/register", function (req, res) {
-  //esta funcion viene directo de la documentacin de bcrypt
-  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
-    const newUser = new User({
-      email: req.body.username,
-      password: hash,
-    });
-
-    /* salvamos los datos del usuario y le damos acceso a /secrets */
-    newUser.save(function (err) {
-      if (err) {
-        console.log(err);
-      } else {
-        res.render("secrets");
-      }
-    });
-  });
-
+  /* Comento toda esta seccion del login porque ahora voy a usar passport */
+  // //esta funcion viene directo de la documentacin de bcrypt
+  // bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+  //   const newUser = new User({
+  //     email: req.body.username,
+  //     password: hash,
+  //   });
+  //   /* salvamos los datos del usuario y le damos acceso a /secrets */
+  //   newUser.save(function (err) {
+  //     if (err) {
+  //       console.log(err);
+  //     } else {
+  //       res.render("secrets");
+  //     }
+  //   });
+  // });
   /********* inicio **********/
   //*Esta parte la estoy comentando porque la estoy mudadando dentro de la funcion bcrypt
   // const newUser = new User({
   //   email: req.body.username,
   //   password: md5(req.body.password), //usamos el hash function para convertir la pass en un hash
   // });
-
   // /* salvamos los datos del usuario y le damos acceso a /secrets */
   // newUser.save(function (err) {
   //   if (err) {
@@ -133,34 +177,64 @@ app.post("/register", function (req, res) {
   //   }
   // });
   /********* end **********/
+
+  /********* registro con passport **********/
+  User.register({ username: req.body.username }, req.body.password, function (
+    err,
+    user
+  ) {
+    if (err) {
+      console.log(err);
+      res.redirect("/register");
+    } else {
+      passport.authenticate("local")(req, res, function () {
+        res.redirect("/secrets");
+      });
+    }
+  });
 });
 /*---------------------------------------------------------*/
 /* creamos el post del login */
 app.post("/login", function (req, res) {
   /* buscamos dentro de la base de datos si los datos ingresados en el form los tenemos */
+  /* Comento toda esta seccion del login porque ahora voy a usar passport */
+  // const username = req.body.username;
+  // const password = req.body.password;
+  // // comento la const con md5 porque ahora usare bcrypt
+  // // const password = md5(req.body.password); //con la funcion md5 comparamos el hash generado en el registro con el hash del login, porque los hash siempre seran iguales.
+  // /* dentro de la bsase, con el metodo propio findOne buscamos el mail del usuario */
+  // User.findOne({ email: username }, function (err, foundUser) {
+  //   if (err) {
+  //     /* si tuvimos un error lo logueamos */
+  //     console.log(err);
+  //   } else {
+  //     if (foundUser) {
+  //       /* sino, revisamos que la pass se corresponda con la almacenada, si corresponde damos acceso a secrets */
+  //       // if (foundUser.password === password) { //comento esta linea porque abajo voy a hacer uso de la funcion brypt
+  //       bcrypt.compare(password, foundUser.password, function (err, result) {
+  //         if (result === true) {
+  //           res.render("secrets");
+  //         }
+  //         // result == true
+  //       });
+  //       // res.render("secrets");
+  //     }
+  //   }
+  // });
 
-  const username = req.body.username;
-  const password = req.body.password;
-  // comento la const con md5 porque ahora usare bcrypt
-  // const password = md5(req.body.password); //con la funcion md5 comparamos el hash generado en el registro con el hash del login, porque los hash siempre seran iguales.
+  /********* login con passport **********/
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password,
+  });
 
-  /* dentro de la bsase, con el metodo propio findOne buscamos el mail del usuario */
-  User.findOne({ email: username }, function (err, foundUser) {
+  req.login(user, function (err) {
     if (err) {
-      /* si tuvimos un error lo logueamos */
       console.log(err);
     } else {
-      if (foundUser) {
-        /* sino, revisamos que la pass se corresponda con la almacenada, si corresponde damos acceso a secrets */
-        // if (foundUser.password === password) { //comento esta linea porque abajo voy a hacer uso de la funcion brypt
-        bcrypt.compare(password, foundUser.password, function (err, result) {
-          if (result === true) {
-            res.render("secrets");
-          }
-          // result == true
-        });
-        // res.render("secrets");
-      }
+      passport.authenticate("local")(req, res, function () {
+        res.redirect("/secrets");
+      });
     }
   });
 });
